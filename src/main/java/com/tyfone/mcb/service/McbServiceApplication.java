8713fba3047b28014f7fff14bcb16c4b09a69abb
sidharth-mishra <mishrasidhartha4@gmail.com>
2017-01-26 16:14:47 +0530

    intial version

---
+++ src/main/java/com/tyfone/mcb/service/McbServiceApplication.java
@@ -0,0 +1,12 @@
+package com.tyfone.mcb.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McbServiceApplication.class, args);
+	}
+}
